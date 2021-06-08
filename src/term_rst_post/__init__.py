a5c46ef7c50afd0479c4e5a77425f7a0a410e41e
Alex Domingo <alex.domingo.toro@vub.be>
2021-06-08 16:34:22 +0200

    use a common version string at install and runtime

---
+++ src/term_rst_post/__init__.py
@@ -0,0 +1,32 @@
+#
+# Copyright 2021-2021 Vrije Universiteit Brussel
+#
+# This file is part of term-rst-post,
+# originally created by the HPC team of Vrije Universiteit Brussel (https://hpc.vub.be),
+# with support of Vrije Universiteit Brussel (https://www.vub.be),
+# the Flemish Supercomputer Centre (VSC) (https://www.vscentrum.be),
+# the Flemish Research Foundation (FWO) (http://www.fwo.be/en)
+# and the Department of Economy, Science and Innovation (EWI) (http://www.ewi-vlaanderen.be/en).
+#
+# https://github.com/vub-hpc/term-rst-post
+#
+# term-rst-post is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation v3.
+#
+# term-rst-post is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this code.  If not, see <http://www.gnu.org/licenses/>.
+#
+##
+"""
+Init term-rst-post
+
+@author: Alex Domingo (Vrije Universiteit Brussel)
+"""
+
+__version__ = "1.1.1"
